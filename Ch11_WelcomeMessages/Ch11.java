bc42280d9b7dc68a1dbb3bf435d8441151fb3fd3
minaashraf30 <mina.ashraf.fouad@gmail.com>
2025-12-16 04:34:56 +0200

    Create Ch11.java

---
+++ Ch11_WelcomeMessages/Ch11.java
@@ -0,0 +1,7 @@
+public class Ch11{
+	public static void main(String args []){
+		System.out.println("welcome to java");
+		System.out.println("welcome to computer science");
+		System.out.println("programming is fun");
+	}
+}
