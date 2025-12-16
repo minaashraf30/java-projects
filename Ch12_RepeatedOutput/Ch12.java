0cefba43bc27b41e381576f96b787d42c3884705
minaashraf30 <mina.ashraf.fouad@gmail.com>
2025-12-16 04:43:33 +0200

    Create Ch12.java

---
+++ Ch12_RepeatedOutput/Ch12.java
@@ -0,0 +1,9 @@
+public class Ch12{
+	public static void main(String args []){
+		System.out.println("welcome to java");
+		System.out.println("welcome to java");
+		System.out.println("welcome to java");
+		System.out.println("welcome to java");
+		System.out.println("welcome to java");
+	}
+}
