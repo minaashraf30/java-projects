38c6ae82bcb22e061bb613e6a8479ef7d7e9196a
minaashraf30 <mina.ashraf.fouad@gmail.com>
2025-12-16 04:48:37 +0200

    Update Ch12.java

--- Ch11_WelcomeMessages/Ch12_RepeatedOutput/Ch12.java
+++ Ch11_WelcomeMessages/Ch12_RepeatedOutput/Ch12.java
@@ -1,9 +1,1 @@
-public class Ch12{
-	public static void main(String args []){
-		System.out.println("welcome to java");
-		System.out.println("welcome to java");
-		System.out.println("welcome to java");
-		System.out.println("welcome to java");
-		System.out.println("welcome to java");
-	}
-}
+
