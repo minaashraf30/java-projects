f16557c5ec341af0d535cc802be32d1e1582d8b4
mina ashraf <mina.ashraf.fouad@gmail.com>
2025-12-17 07:58:40 +0200

    Create Ch15.java

---
+++ Ch15_ExpressionEvaluation/Ch15.java
@@ -0,0 +1,7 @@
+public class Ch15{
+  public static void main(String args []){
+		System.out.println("9.5 X 4.5 - 2.5 X 3");
+		System.out.println("___________________    = " + ((9.5*4.5)-(2.5*3))/(45.5-3.5));
+		System.out.println("     45.5 - 3.5");
+	}
+}
