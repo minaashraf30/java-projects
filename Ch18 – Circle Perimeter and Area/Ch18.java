286ce12514a393f6adc6d06d8731fd919afa0779
mina ashraf <mina.ashraf.fouad@gmail.com>
2025-12-17 08:15:26 +0200

    Create Ch18.java

---
+++ Ch18 – Circle Perimeter and Area/Ch18.java
@@ -0,0 +1,6 @@
+public class Ch18{
+	public static void main(String args []){
+		System.out.println("perimeter = " + (2 * 5.5 * 3.14));
+		System.out.println("area = " + (5.5 * 5.5 * 3.14));	
+	}
+}
