a7802cce6d3835baf32166b5657b261bc827dd65
mina ashraf <mina.ashraf.fouad@gmail.com>
2025-12-19 01:27:51 +0200

    Create Ch112.java

---
+++ Ch112_Average_Speed_Calculation/Ch112.java
@@ -0,0 +1,8 @@
+public class Ch112{
+	public static void main(String args []){
+		double hour = (40.0/60) + (35.0/(60 * 60));
+		double kilo = 24 * 1.6 ;
+		double averageSpeed = kilo / hour ;
+		System.out.println(" average speed : " + averageSpeed);
+	}
+}
