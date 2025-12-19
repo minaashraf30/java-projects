10c30d048885defa4611ba2935b316acf0cc66d6
mina ashraf <mina.ashraf.fouad@gmail.com>
2025-12-19 02:13:01 +0200

    Create Ch113.java

---
+++ Ch113_Linear_Equations_Solver/Ch113.java
@@ -0,0 +1,7 @@
+public class Ch113{
+	public static void main(String args []){
+		double x = ((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1));
+		double y = ((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1));
+		System.out.println(" X is " + x + " and Y is " + y);
+	}
+}
